a21864a2780f0f01c5dae7b7607f33f9b6a833b9
Evymendes <evelyn.laurindo@gmail.com>
2021-11-24 12:45:16 -0300

    CSS adjustment

--- src/pages/activities/enigmaticWord.js
+++ src/pages/activities/enigmaticWord.js
@@ -110,7 +110,7 @@ const Word = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0% 10% 0% 10%;
-  min-width: 2.5rem;
+  min-width: 3rem;
   border-radius: 15px;
   color: #373737;
   font-size: 2rem;
